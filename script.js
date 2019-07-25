446cdd6b54f586838f0cdf5630f764c9ee38f425
Glitch (hello-webpage) <none>
2019-07-25 16:06:33 +0000

    📚🎨 Checkpoint ./script.js:4766309/5659

--- script.js
+++ script.js
@@ -118,11 +118,11 @@ async function main() {
     const url = prompt('What image URL?');
     if (!url) return;
     
-    seedFromImage
+    // restart
     aborted = true;
     i = 0;
     foundClassNames = {};
-    iteration(await (mutate(seedFromImage, {forceClipart:true})));
+    iteration(await fromImageURL(url));
   });
   
 
@@ -140,13 +140,18 @@ function action(foundClassNames, params) {
   return {wat:'diverge', params}
 }
 
-async function mutate(parent, options = {}) {
+function createMutantCanvas() {
   const canvas = document.createElement('canvas');
   canvas.width = 200;
   canvas.height = 200;
   canvas.style.width = '200px';
   canvas.style.height = '200px';
   const ctx = canvas.getContext('2d');
+  return {canvas, ctx};
+}
+
+async function mutate(parent, options = {}) {
+  const {canvas, ctx} = createMutantCanvas();
   
   // ctx.drawImage(img, 0, 0);
   const data = parent.getContext('2d').getImageData(0, 0, 200, 200);
@@ -177,6 +182,33 @@ async function clipartMutation(canvas, ctx) {
   });
 }
 
+async function fromImageURL(url) {
+  const img = new Image();
+  const i = Math.random();
+  return new Promise((resolve, reject) => {
+    img.onload = async function() {
+      status('messing up ur image!');
+      const canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      canvas.style.width = canvas.width + 'px';
+      canvas.style.height = canvas.height + 'px';
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0);
+      document.body.appendChild(canvas);
+      
+      // resample
+      const resampled = createMutantCanvas();
+      resample(resampled.canvas, 200, 200, document.createElement('canvas'));
+      document.body.appendChild(resampled.canvas);
+      resolve(resampled.canvas);
+    }
+    img.crossOrigin = 'Anonymous';
+    status('grabbing your image...');
+    img.src = url;
+  });
+}
+
 // color rectangles
 function rectMutation(canvas, ctx, options = {}) {
   const draws = options.draws || 1;
@@ -195,12 +227,12 @@ function rectMutation(canvas, ctx, options = {}) {
     //   Math.round(Math.random()*255/4) // err towards less strong
     // ]));
     
-    // color drawn from image and modified
+    // color drawn from image and modified (eg, 1/5th)
     const [r,g,b] = sampleColor(canvas, ctx);
     ctx.fillStyle = rgbaify([
-      Math.max(0, Math.min((r-10) + (Math.random()* 20), 255)),
-      Math.max(0, Math.min((g-10) + (Math.random()* 20), 255)),
-      Math.max(0, Math.min((b-10) + (Math.random()* 20), 255)),
+      Math.max(0, Math.min((r-255/10) + (Math.random()* 255/5), 255)),
+      Math.max(0, Math.min((g-255/10) + (Math.random()* 255/5), 255)),
+      Math.max(0, Math.min((b-255/10) + (Math.random()* 255/5), 255)),
       Math.round(Math.random()*255/4) // err towards less strong
     ]);
     
@@ -224,6 +256,92 @@ function sampleColor(canvas, ctx) {
     frame.data[i * 4 + 1],
     frame.data[i * 4 + 2]
   ];
+}
+
+// https://stackoverflow.com/questions/2303690/resizing-an-image-in-an-html5-canvas
+
+/**
+ * Hermite resize - fast image resize/resample using Hermite filter. 1 cpu version!
+ * 
+ * @param {HtmlElement} canvas
+ * @param {int} width
+ * @param {int} height
+ * @param {boolean} resize_canvas if true, canvas will be resized. Optional.
+ */
+function resample(canvas, width, height, resize_canvas) {
+    var width_source = canvas.width;
+    var height_source = canvas.height;
+    width = Math.round(width);
+    height = Math.round(height);
+
+    var ratio_w = width_source / width;
+    var ratio_h = height_source / height;
+    var ratio_w_half = Math.ceil(ratio_w / 2);
+    var ratio_h_half = Math.ceil(ratio_h / 2);
+
+    var ctx = canvas.getContext("2d");
+    var img = ctx.getImageData(0, 0, width_source, height_source);
+    var img2 = ctx.createImageData(width, height);
+    var data = img.data;
+    var data2 = img2.data;
+
+    for (var j = 0; j < height; j++) {
+        for (var i = 0; i < width; i++) {
+            var x2 = (i + j * width) * 4;
+            var weight = 0;
+            var weights = 0;
+            var weights_alpha = 0;
+            var gx_r = 0;
+            var gx_g = 0;
+            var gx_b = 0;
+            var gx_a = 0;
+            var center_y = (j + 0.5) * ratio_h;
+            var yy_start = Math.floor(j * ratio_h);
+            var yy_stop = Math.ceil((j + 1) * ratio_h);
+            for (var yy = yy_start; yy < yy_stop; yy++) {
+                var dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
+                var center_x = (i + 0.5) * ratio_w;
+                var w0 = dy * dy; //pre-calc part of w
+                var xx_start = Math.floor(i * ratio_w);
+                var xx_stop = Math.ceil((i + 1) * ratio_w);
+                for (var xx = xx_start; xx < xx_stop; xx++) {
+                    var dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
+                    var w = Math.sqrt(w0 + dx * dx);
+                    if (w >= 1) {
+                        //pixel too far
+                        continue;
+                    }
+                    //hermite filter
+                    weight = 2 * w * w * w - 3 * w * w + 1;
+                    var pos_x = 4 * (xx + yy * width_source);
+                    //alpha
+                    gx_a += weight * data[pos_x + 3];
+                    weights_alpha += weight;
+                    //colors
+                    if (data[pos_x + 3] < 255)
+                        weight = weight * data[pos_x + 3] / 250;
+                    gx_r += weight * data[pos_x];
+                    gx_g += weight * data[pos_x + 1];
+                    gx_b += weight * data[pos_x + 2];
+                    weights += weight;
+                }
+            }
+            data2[x2] = gx_r / weights;
+            data2[x2 + 1] = gx_g / weights;
+            data2[x2 + 2] = gx_b / weights;
+            data2[x2 + 3] = gx_a / weights_alpha;
+        }
+    }
+    //clear and resize canvas
+    if (resize_canvas === true) {
+        canvas.width = width;
+        canvas.height = height;
+    } else {
+        ctx.clearRect(0, 0, width_source, height_source);
+    }
+
+    //draw
+    ctx.putImageData(img2, 0, 0);
 }
 
 function rgbaify(quad) {
